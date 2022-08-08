cd0497769876b9097c0db02641f971a6231b2caa
przemek-c <105457222+przemek-c@users.noreply.github.com>
2022-08-08 13:13:03 +0200

    Update control_node.py

--- control_node.py
+++ control_node.py
@@ -19,6 +19,21 @@ class MinimalPublisher(Node):
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
+        
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
 
 
 def main(args=None):
