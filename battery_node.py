1c30a9a9d8aba1ac647c90c78e60fdb6e15942d9
przemek-c <105457222+przemek-c@users.noreply.github.com>
2022-08-08 13:07:49 +0200

    Update battery_node.py

--- battery_node.py
+++ battery_node.py
@@ -16,7 +16,7 @@ class MinimalPublisher(Node):
     def timer_callback(self):
         msg = String()
         # msg.data = 'Hello World: %d' % self.i
-        msg.data = 'Battery level: %d %' % self.i
+        msg.data = 'Battery level: %d%%' % self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
