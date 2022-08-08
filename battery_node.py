a5dcf34fb6bf130337d9ab7c8b4d06228fa7a257
przemek-c <105457222+przemek-c@users.noreply.github.com>
2022-08-08 12:59:24 +0200

    Update battery_node.py

--- battery_node.py
+++ battery_node.py
@@ -8,14 +8,15 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'battery_status', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        # msg.data = 'Hello World: %d' % self.i
+        msg.data = 'Battery level: %d %' % self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
